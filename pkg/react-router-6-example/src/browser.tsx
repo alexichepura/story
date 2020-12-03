0555e50de2f0554e63cfe5fe3b43c343a81228be
Alexi Chepura <a.chepura@gmail.com>
2020-12-03 21:12:29 +0300

    install rr6

---
+++ pkg/react-router-6-example/src/browser.tsx
@@ -0,0 +1,43 @@
+import { createBrowserHistory } from "history"
+import React from "react"
+import { render } from "react-dom"
+import { Router } from "react-router"
+import { Story } from "story"
+import { DataRoutes, Preloader } from "story-react-router-5"
+import { createBranchItemMapper, getBranch, routes, StoryContext, TAppBranchItem } from "./app"
+import { DbClient } from "./db"
+
+const init = async () => {
+  const history = createBrowserHistory()
+  const deps = { apiSdk: new DbClient() }
+  const story: Story = new Story({
+    branchItemsMapper: (branchItem, abortController) =>
+      createBranchItemMapper(story, deps)(branchItem as TAppBranchItem, abortController),
+    data: window.ssr_data,
+    statusCode: window.ssr_statusCode,
+    onLoadError: (err) => {
+      console.log("onLoadError", err)
+    },
+  })
+  const { pathname } = history.location
+  await story.loadData(getBranch(routes, pathname), pathname)
+  const el = document.getElementById("app")
+  render(
+    <StoryContext.Provider value={story}>
+      <Router history={history}>
+        <Preloader routes={routes} getBranch={getBranch} story={story}>
+          <DataRoutes routes={routes} story={story} />
+        </Preloader>
+      </Router>
+    </StoryContext.Provider>,
+    el
+  )
+}
+init()
+
+declare global {
+  interface Window {
+    ssr_data: any
+    ssr_statusCode: number
+  }
+}
