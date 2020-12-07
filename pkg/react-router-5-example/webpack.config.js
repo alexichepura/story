bebb21828e828a78e7d243808524922ce9541a4e
Alexi Chepura <a.chepura@gmail.com>
2020-12-07 12:50:58 +0300

    upgrade deps

--- pkg/react-router-5-example/webpack.config.js
+++ pkg/react-router-5-example/webpack.config.js
@@ -1,87 +1,9 @@
-// require("dotenv").config()
-// import { join } from "path"
-// import { Configuration as WebpackConfiguration } from "webpack"
-// import { Configuration as WebpackDevServerConfiguration } from "webpack-dev-server"
-// import { env } from "./env"
-
-// interface Configuration extends WebpackConfiguration {
-//   devServer?: WebpackDevServerConfiguration
-// }
-
-// const { WDS_PORT } = env
-
-// const ROOT_DIR = join(__dirname, "..")
-// const EXAMPLE_DIR = join(ROOT_DIR, "example")
-// const EXAMPLE_DIST_DIR = join(EXAMPLE_DIR, "dist")
-// // const SRC_DIR = join(ROOT_DIR, "src")
-// const PUBLIC_OUTPUT_PATH = WDS_PORT ? `http://localhost:${WDS_PORT}/dist/` : "/dist/"
-
-// const config: Configuration = {
-//   entry: {
-//     browser: EXAMPLE_DIR + "/browser.tsx",
-//   },
-//   devServer: {
-//     contentBase: false,
-//     disableHostCheck: true,
-//     publicPath: PUBLIC_OUTPUT_PATH,
-//     compress: false,
-//     port: Number(WDS_PORT),
-//   },
-//   devtool: "cheap-module-eval-source-map",
-//   mode: "development",
-//   module: {
-//     rules: [
-//       {
-//         test: /\.tsx?$/,
-//         use: {
-//           loader: "ts-loader",
-//         },
-//       },
-//     ],
-//   },
-//   output: {
-//     filename: `[name].js`,
-//     chunkFilename: `[name].js`,
-//     path: EXAMPLE_DIST_DIR,
-//     publicPath: PUBLIC_OUTPUT_PATH,
-//   },
-//   resolve: {
-//     extensions: [".ts", ".tsx", ".js"],
-//     symlinks: false,
-//   },
-// }
-
-// export default config
-
 // @ts-check
 require("dotenv").config()
 const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin")
-// const WebpackAssetsManifest = require("webpack-assets-manifest")
 
 const { join, resolve } = require("path")
 const { DefinePlugin } = require("webpack")
-
-// class Entrypoints {
-//   /**
-//    * @param {import('webpack').Compiler} [compiler]
-//    */
-//   apply(compiler) {
-//     compiler.hooks.emit.tapAsync("Entrypoints", ({ entrypoints, assets }, callback) => {
-//       const entrypointsAppFiles = entrypoints.get("app").getFiles()
-
-//       // @ts-ignore
-//       assets["entrypoints.json"] = {
-//         source: function () {
-//           return JSON.stringify(entrypointsAppFiles)
-//         },
-//         size: function () {
-//           return entrypointsAppFiles.length
-//         },
-//       }
-//       callback()
-//     })
-//   }
-// }
 
 const { NODE_ENV, WDS_PORT } = process.env
 const IS_PROD = NODE_ENV === "production"
